052bd393dd5d5ef4c38afbfa6689cd23e8ab646c
SantanaDZ <andresantmartins@outlook.com>
2025-02-16 11:04:46 -0300

    Add files via upload

---
+++ Aula 01/main.py
@@ -0,0 +1,33 @@
+# nome = input("Digite seu nome:")
+# idade = input("Digite sua idade:")
+# altura = input("Digite sua altura:")
+
+# print(nome)
+# print(idade)
+# print(altura)
+
+# print(type(nome))
+# print(type(idade))
+# print(type(altura))
+
+
+nome = input("Digite seu nome:")
+salario = input("Digite seu salário:")
+situacao_de_emprego = input("Digite sua situação de emprego:(Empregado ou Desempregado)")
+cpf=input("Digite seu CPF:")
+
+print(nome)
+print(salario)
+print(situacao_de_emprego)
+print(cpf)
+
+print(type(nome))
+print(type(salario))
+print(type(situacao_de_emprego))
+print(type(cpf))
+
+
+
+
+
+
