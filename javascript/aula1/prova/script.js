614f71540c432ca26d1d9678700da6c4808edeb1
SantanaDZ <andresantmartins@outlook.com>
2025-11-30 10:59:37 -0300

    Add files via upload

---
+++ javascript/aula1/prova/script.js
@@ -0,0 +1,18 @@
+let num1 = Number(prompt("Digite o primeiro número: "));
+let num2 = Number(prompt("Digite o segundo número: "));
+
+let resultado = num1;
+resultado += num2;
+console.log("Resultado da adição: " + resultado);
+
+resultado = num1;
+resultado *= num2;
+console.log("Resultado da multiplicação: " + resultado);
+
+resultado = num1;
+resultado /= num2; 
+console.log("Resultado da divisão: " + resultado);
+
+resultado = num1;
+resultado %= num2; 
+console.log("Resultado do resto da divisão: " + resultado);
