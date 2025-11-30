67ac3ff72b45a189631c4fb3e2f34ba14d2d50da
SantanaDZ <andresantmartins@outlook.com>
2025-11-30 12:27:19 -0300

    Add files via upload

---
+++ javascript/aula3/prova/scrip.js
@@ -0,0 +1,18 @@
+let nomes = []
+while (true){
+    let entrada = prompt("Digite um nome (ou 'sair' para encerrar): ");
+    if(entrada.toLowerCase()=== 'sair'){
+        break;
+    }
+    nomes.push(entrada)
+}
+
+console.log("\n=== Lista de nomes com índices ===");
+for (let i = 0; i< nomes.length; i++){
+    console.log(`${i+1}: ${nomes[i]}`)
+}
+
+console.log("\n=== Mensagens personalizadas ===");
+for(let nome of nomes){
+    console.log(`Bem-vindo(a), ${nome}!`)
+}
